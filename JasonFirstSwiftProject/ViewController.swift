941bade178db06d4309ba0b6cf7e5629f3f1770e
Capitl-IT <jason@huangyutangde-macbook-pro.local>
2018-07-09 19:37:08 +0800

    Initial Commit

---
+++ JasonFirstSwiftProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JasonFirstSwiftProject
+//
+//  Created by jason on 2018/7/9.
+//  Copyright © 2018年 jason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
